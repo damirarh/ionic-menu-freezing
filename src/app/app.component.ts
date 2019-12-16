ab60668f655a37e68825da36b48cc45f7023887d
Damir Arh <damir.arh@gmail.com>
2019-12-16 07:36:58 +0100

    Menu freezes if Close clicked before animation end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, MenuController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
@@ -26,7 +26,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private menuCtrl: MenuController
   ) {
     this.initializeApp();
   }
@@ -36,5 +37,10 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  async closeMenu() {
+    await this.menuCtrl.close();
+    await this.menuCtrl.enable(false);
   }
 }
